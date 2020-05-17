2fa14abd46ac2c8c2f2832fd8bbc5069b18ae53c
HK Verma <fpgacompiler@gmail.com>
2020-05-16 21:35:21 -0700

    Added BuidlUtils.cmake

---
+++ cpp/BuildUtils/BuildUtils.cmake
@@ -0,0 +1,6 @@
+#Apache Arrow
+# installs in /usr/local/include & /usr/local/lib
+#set(APACHE_ARROW_CPP $ENV{TENDB_ROOT_DIR}/cpp/external_libs/arrow/arrow/cpp)
+#link_directories(AFTER ${APACHE_ARROW_CPP}/release/release)
+#include_directories(${APACHE_ARROW_CPP}/src)
+
