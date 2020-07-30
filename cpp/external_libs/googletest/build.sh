4ae9c23cb7efe1ab4e4bd1cb6375e5f56f2ba9ce
Ubuntu <azureuser@dev.udukqz5jp4je5ng1bvoq2ijmdd.xx.internal.cloudapp.net>
2020-07-30 04:26:32 +0000

    Added release & debug build options

--- cpp/external_libs/googletest/build.sh
+++ cpp/external_libs/googletest/build.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+# If nothing provided build debug
+build_target="Debug"
+if [[ "$#" -gt 0 ]]; then
+    if [[ "$1" == "Debug" || "$1" == "Release" ]]; then
+        build_target=$1
+    else
+        echo "Usage: build.sh [Debug|Release]"
+        exit
+    fi
+fi
 pushd googletest
-mkdir -p debug
-pushd debug
-cmake .. -DCMAKE_BUILD_TYPE=Debug
-make
-sudo make install
-popd
+if [[ ${build_target} = "Release" ]]; then
+    echo "Building release.."
+    mkdir -p release
+    pushd release
+    cmake .. -DCMAKE_BUILD_TYPE=Release
+    make
+    sudo make install
+    popd
+else
+    mkdir -p debug
+    pushd debug
+    cmake .. -DCMAKE_BUILD_TYPE=Debug
+    make
+    sudo make install
+    popd
+fi
 popd
