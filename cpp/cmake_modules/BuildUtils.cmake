6096062247c9c580aae3312c15fd843ce8a57e1e
HK Verma <paccelpla@gmail.com>
2020-12-13 19:05:30 -0800

    setup.py runs but no cmake modules built

---
+++ cpp/cmake_modules/BuildUtils.cmake
@@ -0,0 +1,50 @@
+# TODO create debug/release directory
+#
+# Set Versions
+#
+message(STATUS "Building using CMake version: ${CMAKE_VERSION}")
+#
+# Set TenDB Version
+#
+set(TENDB_VERSION "0.0.1")
+string(REGEX MATCH "^[0-9]+\\.[0-9]+\\.[0-9]+" TENDB_BASE_VERSION "${TENDB_VERSION}")
+#
+# Select C++17
+#
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+#
+# if no build build type is specified, default to debug builds
+#
+if(NOT DEFINED CMAKE_BUILD_TYPE)
+  set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build.")
+endif()
+#
+# Set global compile options
+#
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+#
+# TenDB RootDir
+#
+set (TENDB_ROOT_DIR $ENV{TENDB_ROOT_DIR})
+
+# Bin directories
+#
+set(TENDB_BIN ${TENDB_ROOT_DIR}/build/bin)
+#
+# TBB include directory
+#
+set(TBB_INC_DIR $ENV{TENDB_ROOT_DIR}/cpp/external_libs/tbb/tbb/include)
+set(TBB_LIB_DIR $ENV{TENDB_ROOT_DIR}/cpp/external_libs/tbb/bin)
+#
+# Source Code directories
+#
+set(COMMON_SRC  $ENV{TENDB_ROOT_DIR}/cpp/common)
+set(DTENSOR_SRC $ENV{TENDB_ROOT_DIR}/cpp/dtensor)
+set(DTENSOR_TEST_SRC $ENV{TENDB_ROOT_DIR}/cpp/dtensor_test)
+#
+# Global include and link directories
+# TODO expand include directories
+#
+include_directories(/usr/include/python3.6)
+link_directories(${TBB_LIB_DIR})
--- py/tendb/setup.py
+++ py/tendb/setup.py
@@ -1,24 +1,174 @@
-import setuptools
+import contextlib
+import os
+import os.path
+from os.path import join as pjoin
+import re
+import shlex
+import shutil
+import sys
 
 from Cython.Distutils import build_ext as _build_ext
 import Cython
-from setuptools import setup, Extension
+from setuptools import setup, Extension, Distribution
+from distutils import sysconfig
 
 #from distutils.command.build_ext import build_ext
 
 # Based on https://github.com/libdynd setup.py & arrow setup.py
 
+# Check if we're running 64-bit Python
+is_64_bit = sys.maxsize > 2**32
+
+if Cython.__version__ < '0.29':
+    raise Exception('Please upgrade to Cython 0.29 or newer')
+
+setup_dir = os.path.abspath(os.path.dirname(__file__))
+
+ext_suffix = sysconfig.get_config_var('EXT_SUFFIX')
+if ext_suffix is None:
+    # https://bugs.python.org/issue19555
+    ext_suffix = sysconfig.get_config_var('SO')
+
+@contextlib.contextmanager
+def changed_dir(dirname):
+  oldcwd = os.getcwd()
+  os.chdir(dirname)
+  try:
+    yield
+  finally:
+    os.chdir(oldcwd)
+
 class cmake_build_ext(_build_ext):
   description = "Build the C-extension for dynd-python with CMake"
   user_options = [('extra-cmake-args=', None, 'extra arguments for CMake')]
 
+  CYTHON_MODULE_NAMES = [
+    'lib',
+    '_tendb']
+
+
+  def initialize_options(self):
+    _build_ext.initialize_options(self)
+    self.build_type = os.environ.get('TENDB_BUILD_TYPE','debug').lower()
+
   def run(self):
-    global build_type
-    
+    self._run_cmake()
+    _build_ext.run(self)
+
+  def get_ext_built(self, name):
+    return pjoin(self.build_type, name + ext_suffix)
+
+  # TODO Disallow failure
+  def _failure_permitted(self, name):
+    return True
+
+  def _run_cmake(self):
+    # check if build_type is correctly passed / set
+    if self.build_type.lower() not in ('release', 'debug'):
+      raise ValueError("--build-type needs to be 'release' or 'debug'")
+
+    # The directory containing this setup.py
+    source = os.path.dirname(os.path.abspath(__file__))
+
+    # The staging directory for the module being built
+    build_cmd = self.get_finalized_command('build')
+    build_temp = pjoin(os.getcwd(), build_cmd.build_temp)
+    build_lib = pjoin(os.getcwd(), build_cmd.build_lib)
+    saved_cwd = os.getcwd()
+
+    if not os.path.isdir(build_temp):
+      self.mkpath(build_temp)
+
+    # Change to the build directory
+    with changed_dir(build_temp):
+      # Detect if we built elsewhere
+      if os.path.isfile('CMakeCache.txt'):
+        cachefile = open('CMakeCache.txt', 'r')
+        cachedir = re.search('CMAKE_CACHEFILE_DIR:INTERNAL=(.*)',
+                             cachefile.read()).group(1)
+        cachefile.close()
+        if (cachedir != build_temp):
+          return
+
+      static_lib_option = ''
+
+      cmake_options = [
+        '-DPYTHON_EXECUTABLE=%s' % sys.executable,
+        '-DPython3_EXECUTABLE=%s' % sys.executable,
+        static_lib_option,
+      ]
+
+      def append_cmake_bool(value, varname):
+        cmake_options.append('-D{0}={1}'.format(
+          varname, 'on' if value else 'off'))
+
+      cmake_options.append('-DCMAKE_BUILD_TYPE={0}'
+                           .format(self.build_type.lower()))
+
+      extra_cmake_args = []
+      build_tool_args = []
+
+      # Generate the build files
+      print("-- Running cmake for tendb")
+      self.spawn(['cmake'] + extra_cmake_args + cmake_options + [source])
+      print("-- Finished cmake for tendb")
+
+      print("-- Running cmake --build for tendb")
+      self.spawn(['cmake', '--build', '.', '--config', self.build_type] +
+                 build_tool_args)
+      print("-- Finished cmake --build for tendb")
+
+      if self.inplace:
+        # a bit hacky
+        build_lib = saved_cwd
+
+      # Move the libraries to the place expected by the Python build
+      try:
+        os.makedirs(pjoin(build_lib, 'tendb'))
+      except OSError:
+        pass
+
+      if sys.platform == 'win32':
+        build_prefix = ''
+      else:
+        build_prefix = self.build_type
+
+
+      # Move the built C-extension to the place expected by the Python build
+      self._found_names = []
+      for name in self.CYTHON_MODULE_NAMES:
+        built_path = self.get_ext_built(name)
+        if not os.path.exists(built_path):
+          print('Did not find {0}'.format(built_path))
+          if self._failure_permitted(name):
+            print('Cython module {0} failure permitted'
+                  .format(name))
+            continue
+          raise RuntimeError('tendb C-extension failed to build:',
+                             os.path.abspath(built_path))
+
+        # The destination path to move the built C extension to
+        ext_path = pjoin(build_lib, self._get_cmake_ext_path(name))
+        if os.path.exists(ext_path):
+          os.remove(ext_path)
+        self.mkpath(os.path.dirname(ext_path))
+
+        if self.bundle_cython_cpp:
+          self._bundle_cython_cpp(name, build_lib)
+
+        print('Moving built C-extension', built_path,
+              'to build path', ext_path)
+        shutil.move(built_path, ext_path)
+        self._found_names.append(name)
+
+        if os.path.exists(self.get_ext_built_api_header(name)):
+          shutil.move(self.get_ext_built_api_header(name),
+                      pjoin(os.path.dirname(ext_path), name + '_api.h'))
+
 with open("README.md", "r") as fh:
   long_description = fh.read()
 
-setuptools.setup(
+setup(
   name='tendb',
   version='0.0.1',
   author='HK Verma',
@@ -35,14 +185,14 @@ setuptools.setup(
   #    distclass=BinaryDistribution,
   # build_ext is overridden to call cmake, the Extension is just
   # needed so things like bdist_wheel understand what's going on
-  ext_modules=[Extension('tendb', sources=["includes/__init__.pxd","includes/common.pxd","includes/libarrow.pxd"])],
+  ext_modules=[Extension('tendb', sources=[])],
   # This includes both build and install requirements. Setuptools' setup_requires
   # option does not actually install things, so isn't actually helpful...
     install_requires = ['cython'],
   cmdclass={
     'build_ext': cmake_build_ext
   },
-  
+
   classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
