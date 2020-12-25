ae48cd4b5a70779e6b083bf76e4560103cc60410
HK Verma <paccelpla@gmail.com>
2020-12-25 12:54:13 -0800

    Consolidated Tpchdemo

--- cpp/dtensor_test/TestMain.cpp
+++ cpp/dtensor_test/TestMain.cpp
@@ -5,21 +5,14 @@
 
 #include "common.h"
 #include "dtensor.h"
-#include "TpchQueries.h"
+#include "TpchDemo.h"
 
 #include <tbb/tbb.h>
 
 using namespace tendb;
+using namespace std;
 
 int main(int argc, char** argv) {
-
-  /*
-  tbb::parallel_invoke
-    ([]() { std::cout << "Hello " << std::endl; },
-     []() { std::cout << "Hello " << std::endl; }
-     );
-  return 0;
-  */
 
   if (argc < 2) {
     std::cout << "Usage: exec file_name" << std::endl;
@@ -27,88 +20,52 @@ int main(int argc, char** argv) {
   }
   std::string tpchDataDir = argv[1];
 
-  // Initialize Google's logging library.
+  // Get Cache and Demo objects
   std::shared_ptr<TCache>  tCache = TCache::GetInstance();
+  shared_ptr<TpchDemo> tpchDemo = TpchDemo::GetInstance(tCache);
 
-  TpchQueries tpchQueries(tCache, tpchDataDir);
-  tpchQueries.ReadTables();
-  tpchQueries.PrintSchemas();
+  // Read tpch tables
+  tpchDemo->ReadTables(tpchDataDir);
+  tpchDemo->PrintSchemas();
 
+  // Run Query6
   StopWatch stopWatch;
   stopWatch.Start();
-  double result = tpchQueries.Query6Serial();
+  double result = tpchDemo->Query6Serial();
   stopWatch.Stop();
   LOG(INFO) << "Query6 Revenue=" << result;
   LOG(INFO) << stopWatch.ElapsedInMicroseconds() << "us";
 
   stopWatch.Start();
-  result = tpchQueries.Query6Parallel();
+  result = tpchDemo->Query6();
   stopWatch.Stop();
   LOG(INFO) << "Query6 Revenue=" << result;
   LOG(INFO) << stopWatch.ElapsedInMicroseconds() << "us";
 
   stopWatch.Start();
-  bool mapResult = tpchQueries.MakeMaps();
+  bool mapResult = tpchDemo->MakeMaps();
   stopWatch.Stop();
   LOG(INFO) << (mapResult?"Success":"Failed ") << " Column maps.";
   LOG(INFO) << "TenDB Tensor Creation Time (us)=" << stopWatch.ElapsedInMicroseconds();
-  tpchQueries.PrintMaps();
+  tpchDemo->PrintMaps();
 
-  double revenue[25] = {0};
-  auto printRevenue = [&]() -> void {
-    const char* nations[25] = {
-      "ALGERIA",
-      "ARGENTINA",
-      "BRAZIL",
-      "CANADA",
-      "EGYPT",
-      "ETHIOPIA",
-      "FRANCE",
-      "GERMANY",
-      "INDIA",
-      "INDONESIA",
-      "IRAN",
-      "IRAQ",
-      "JAPAN",
-      "JORDAN",
-      "KENYA",
-      "MOROCCO",
-      "MOZAMBIQUE",
-      "PERU",
-      "CHINA",
-      "ROMANIA",
-      "SAUDI ARABIA",
-      "VIETNAM",
-      "RUSSIA",
-      "UNITED KINGDOM",
-      "UNITED STATES"
-    };
-    int64_t russia=22, romania=19, uk=23, france=6, germany=7;
+  auto printRevenue = [&](std::map<std::string, double>& q5revs) -> void {
     LOG(INFO) << "Query5 Revenue=";
-    LOG(INFO) << nations[russia] << "=" << revenue[russia];
-    LOG(INFO) << nations[romania] << "=" << revenue[romania];
-    LOG(INFO) << nations[uk] << "=" << revenue[uk];
-    LOG(INFO) << nations[france] << "=" << revenue[france];
-    LOG(INFO) << nations[germany] << "=" << revenue[germany];
-
-    LOG(INFO) << "All nations Revenue=";
-    for (int i=0 ; i< 25; i++) {
-      LOG(INFO) << nations[i] << "=" << revenue[i];
+    for (auto nrev: q5revs) {
+      LOG(INFO) << nrev.first << "=" << nrev.second;
     }
   };
 
   stopWatch.Start();
-  tpchQueries.Query5Serial(revenue);
+  auto q5revs = tpchDemo->Query5();
   stopWatch.Stop();
-  printRevenue();
+  printRevenue(q5revs);
   LOG(INFO) << stopWatch.ElapsedInMicroseconds() << "us";
 
-  for (int i=0; i<25; i++)
-    revenue[i] = 0;
   stopWatch.Start();
-  tpchQueries.Query5Parallel(revenue);
+  q5revs = tpchDemo->Query5Serial();
   stopWatch.Stop();
-  printRevenue();
+  printRevenue(q5revs);
   LOG(INFO) << stopWatch.ElapsedInMicroseconds() << "us";
 
   return EXIT_SUCCESS;
--- cpp/dtensor_test/TpchDemo.h
+++ cpp/dtensor_test/TpchDemo.h
@@ -3,31 +3,119 @@
 namespace tendb
 {
 
+  // TODO expose numWorkers to the demo same as from TCache
+
   class TpchDemo
   {
   public:
 
     static std::shared_ptr<TpchDemo> GetInstance(std::shared_ptr<TCache> tCache);
-    
-    TpchDemo(std::shared_ptr<TCache> tCache) : tCache_(tCache) { }
+    TpchDemo(std::shared_ptr<TCache> tCache) :
+      tCache_(tCache) { }
 
-    // lineitem tables
-    std::shared_ptr<arrow::ChunkedArray> lShipdate, lDiscount, lQuantity, lExtendedprice;    
+    // TPCH table enums
+    enum {
+      lineitem=0,
+      customer=1,
+      orders=2,
+      supplier=3,
+      nation=4,
+      region=5,
+      numTables=6
+    };
+
+    std::vector<std::string> tableNames =
+      {
+        "lineitem",
+        "customer",
+        "orders",
+        "supplier",
+        "nation",
+        "region"
+      };
+
+    // Table entries
+    const static int32_t c_custkey=0, c_nationkey=3;
+    const static int32_t o_custkey=1, o_orderkey=0, o_orderdate=4;
     const static int32_t l_orderkey=0, l_suppkey=2, l_shipdate=10,
       l_discount=6, l_quantity=4, l_extendedprice=5;
+    const static int32_t s_suppkey=0, s_nationkey=3;
+    const static int32_t n_nationkey=0, n_regionkey=2, n_name=1;
+    const static int32_t r_regionkey=0, r_name=1;
 
+    // lineitem tables
+    std::shared_ptr<arrow::ChunkedArray> lShipdate, lDiscount, lQuantity, lExtendedprice;
+    std::shared_ptr<arrow::ChunkedArray> lOrderkey, lSuppkey;
+
+    // Read tables for Query6 and Query5
+    void ReadTables(std::string tpchDir);
+
+    // Run Query6
     double Query6();
-    void Query6Chunk(int64_t chunkNum, double& revenue);
+    double Query6Serial();
+    // Get Query6 revenue for chunk chunkNum
+    void GetQuery6Revenue(int64_t chunkNum, double& revenue);
 
-    static std::shared_ptr<TpchDemo> tpchDemo_;
+    // Run Query5
+    std::map<std::string, double> Query5();
+    std::map<std::string, double> Query5Serial();
+    // Get Query5 revenue for chunk chunkNum
+    void GetQuery5Revenue(int64_t chunkNum, double revenue[], int32_t mapNum);
+
+    // Create TenDB maps
+    bool MakeMaps();
+
+    void PrintSchemas();
+    void PrintMaps();
+
+    std::vector<std::shared_ptr<TTable>> tables_;
     std::shared_ptr<TCache> tCache_;
-    std::shared_ptr<TTable> lineitem_;
 
+    const int64_t rowIncrementsForTimeLog = 500000;
     int64_t date19970101Value;
     int64_t date19971231Value;
-    
-    // Num threads
-    static const int64_t numParallels_ = 6;
+    int64_t date19950101Value;
+    int64_t date19951231Value;
+
+    static std::shared_ptr<TpchDemo> tpchDemo_;
+
+    // Total worker numbers
+    static const int64_t numWorkers_ = 6;
+    static const int64_t numMaps_ = 1;
+
+    // Store TpchData properties
+    static const int numNations = 25;
+    inline static const std::string nations[numNations] = {
+      "ALGERIA",
+      "ARGENTINA",
+      "BRAZIL",
+      "CANADA",
+      "EGYPT",
+      "ETHIOPIA",
+      "FRANCE",
+      "GERMANY",
+      "INDIA",
+      "INDONESIA",
+      "IRAN",
+      "IRAQ",
+      "JAPAN",
+      "JORDAN",
+      "KENYA",
+      "MOROCCO",
+      "MOZAMBIQUE",
+      "PERU",
+      "CHINA",
+      "ROMANIA",
+      "SAUDI ARABIA",
+      "VIETNAM",
+      "RUSSIA",
+      "UNITED KINGDOM",
+      "UNITED STATES"
+    };
+    // Query5 revenues
+    double q5revenues[25];
+    void ClearQ5Revenues();
+    std::map<std::string, double> GetAggrRevenues();
 
   };
 
