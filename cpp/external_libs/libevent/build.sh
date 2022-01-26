cb457358b6714c75d34bf7cdea2b25b30db9619d
HK Verma <paccelpla@gmail.com>
2022-01-25 22:09:07 -0800

    new libs

---
+++ cpp/external_libs/libevent/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# If nothing provided build debug
+#
+if [ -z "$LITEN_BUILD_TYPE" ]; then
+    echo "LITEN_BUILD_TYPE must be set"
+    exit
+fi
+build_type=${LITEN_BUILD_TYPE}
+pushd libevent
+if [[ ${build_type} = "release" ]]; then
+    echo "Building release.."
+    mkdir -p release
+    pushd release
+    cmake .. -DCMAKE_BUILD_TYPE=Release
+    make
+    popd
+else
+    mkdir -p debug
+    pushd debug
+    cmake .. -DCMAKE_BUILD_TYPE=Debug
+    make
+    popd
+fi
+popd
