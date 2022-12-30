4f91b4c47e3c436803d1731a12058fde799551e0
HK Verma <paccelpla@gmail.com>
2022-12-29 18:17:47 -0800

    Updated buildall

--- cpp/opensource/buildall.sh
+++ cpp/opensource/buildall.sh
@@ -1,36 +1,54 @@
 #!/bin/bash
 # Build opensource libraries
 #
-# If nothing provided build debug
+
+if [ -z "$LITEN_BUILD_TYPE" ]; then
+    echo "LITEN_BUILD_TYPE must be set"
+    exit
+fi
+build_type=${LITEN_BUILD_TYPE}
+
+if [[ "${build_type}" == "debug" || "${build_type}" == "release" ]]; then
+    echo "Building ${build_type}"
+else
+    echo "LITEN_BUILD_TYPE must be release or debug"
+    exit
+fi
+# Build all in build_type directory
+mkdir -p ${build_type}
+pushd ${build_type}
+
 # Build folly - Its various dependencies are listed here - folly/build/fbcode_builder/manifests/folly
 # buildvm.sh has the apt-get install command to install all the dependencies
-mkdir p folly/build_
-pushd folly/build_
-cmake ..
+mkdir -p folly
+pushd folly
+cmake ../../folly
 make -j $(nproc)
 sudo make install
 popd
 # Build fizz
-mkdir -p fizz/build_
-pushd fizz/build_
-cmake ../fizz
+mkdir -p fizz
+pushd fizz
+cmake ../../fizz/fizz
 make -j $(nproc)
 sudo make install
 popd
 # Build wangle
-mkdir -p wangle/build_
-pushd wangle/build_
+mkdir -p wangle
+pushd wangle
+cmake ../../wangle/wangle
+make -j $(nproc)
+sudo make install
+popd
+#
+# Build arrow
+rm -rf arrow
+mkdir -p arrow
+pushd arrow
+cmake ../../arrow/cpp -DCMAKE_BUILD_TYPE="${build_type}" -DARROW_CSV=ON -DARROW_FILESYSTEM=ON -DARROW_PYTHON=ON
 make -j $(nproc)
 sudo make install
 popd
 
-
-
-#for dir in glog googletest double-conversion libevent nlohmann folly tbb re2 arrow velox ray
-#for dir in gflags glog tbb re2 nlohmann arrow
-#do           
-#    echo "Building ${dir} ... "
-#    pushd $dir
-#    ./build.sh
-#    popd
-#done
+# get out of build dir
+popd
