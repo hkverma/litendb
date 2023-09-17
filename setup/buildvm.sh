9fffab475ed153ffb2552de39c0fbf75d2f2df93
HK Verma <paccelpla@gmail.com>
2023-09-16 20:27:13 -0700

    Can compile C++, need python compile fixes

--- setup/buildvm.sh
+++ setup/buildvm.sh
@@ -99,14 +99,39 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
 #Install node.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-npm install --lts     
-npm install node
-npm install -g typescript
-npm install -g ts-node
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+# npm install --lts     
+# npm install node
+# npm install -g typescript
+# npm install -g ts-node
 
-#pyspark & java
-sudo apt install openjdk-17-jdk openjdk-17-jre
+# install jdk
+sudo apt install -y openjdk-17-jdk openjdk-17-jre
+# install python - keep default python (3.10) for Ubuntu22
+sudo apt install python3-pip
+alias python=python3
+pip install numpy
+pip install cython==0.29.30
 pip install jupyterlab
-pip install pyspark=3.3.2
+pip install pyspark=3.2.4
 pip install seaborn
+
+#Install arrow dev For now, build it in opensources
+#sudo apt update
+#sudo apt install -y -V ca-certificates lsb-release wget
+#wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
+#sudo apt install -y -V ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
+#sudo apt update
+#sudo apt install -y -V libarrow-dev # For C++
+#sudo apt install -y -V libarrow-glib-dev # For GLib (C)
+#sudo apt install -y -V libarrow-dataset-dev # For Apache Arrow Dataset C++
+#sudo apt install -y -V libarrow-dataset-glib-dev # For Apache Arrow Dataset GLib (C)
+#sudo apt install -y -V libarrow-acero-dev # For Apache Arrow Acero
+#sudo apt install -y -V libarrow-flight-dev # For Apache Arrow Flight C++
+#sudo apt install -y -V libarrow-flight-glib-dev # For Apache Arrow Flight GLib (C)
+#sudo apt install -y -V libarrow-flight-sql-dev # For Apache Arrow Flight SQL C++
+#sudo apt install -y -V libarrow-flight-sql-glib-dev # For Apache Arrow Flight SQL GLib (C)
+##sudo apt install -y -V libgandiva-dev  For Gandiva C++
+##sudo apt install -y -V libgandiva-glib-dev  For Gandiva GLib (C)
+#sudo apt install -y -V libparquet-dev # For Apache Parquet C++
+#sudo apt install -y -V libparquet-glib-dev # For Apache Parquet GLib (C)
